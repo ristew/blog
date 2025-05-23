f4374155da68f3b9ca9a9db7903b0901d73c8d39
Riley Stewart <me@rileystew.art>
2025-05-23 15:14:35 -0700

    move posts for out

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 rm -rf out
 cp -r public out
 bun run template_posts.js
-bun build src/*.html --outdir=out --sourcemap=linked
-bun build posts/*.html --outdir=out/posts --sourcemap=linked
+bun build src/*.html src/posts/*.html --outdir=out --sourcemap=linked
